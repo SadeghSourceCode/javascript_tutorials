81606753f9d31247a2826c6f13188e51bdd8cd9d
SadeghSourceCode <sadegh.source.code@gmail.com>
2023-09-04 22:23:22 +0330

    Chapter8

---
+++ Chapter8/index.js
@@ -0,0 +1,43 @@
+// let jsonData = '{"userId": 1,"id": 1,"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto","createdAt":"2023-08-01T15:22:56.6333333"}';
+// let post = JSON.parse(jsonData);
+// let postDate = new Date(post.createdAt);
+// let dateElm = document.getElementById('date');
+// dateElm.innerText = HMofDate(postDate)
+
+// function stringDate(date) {
+//     return date.toLocaleDateString().replaceAll("/", " ")
+// }
+
+// function MDHofDate(date) {
+//     let month = date.getMonth();
+//     let day = date.getDay();
+//     let hours = date.getHours();
+//     return `${month} ${day} ${hours}`;
+// }
+
+// function HMofDate(date) {
+//     let hours = date.getHours();
+//     let minutes = date.getMinutes();
+//     return `${hours}:${minutes}`;
+// }
+
+
+// "2023-08-01T12:00:00"
+// "2023-08-02T12:00:00"
+
+// let date1 = new Date('2023-08-01T12:00:00').getTime();
+// let date2 = new Date('2023-08-02T12:00:00').getTime();
+
+// let result = date1 - date2;
+
+// if (result > 0) {
+//     // عدد اول بزرگتر از عدد دوم
+//     alert('عدد اول بزرگتر از عدد دوم')
+// }
+// else {
+//     // عدد دوم بزرگتر از عدد اول
+//     alert('عدد دوم بزرگتر از عدد اول')
+// }
+
+// alert(Math.abs(result) / 1000 / 60 / 60 + 'میزان اختلاف به ساعت')
+
